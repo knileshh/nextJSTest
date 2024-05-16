c61e38b58e287c48872f4ae770a3223460841ee7
knilesh9 <neutron_stays@protonmail.com>
2024-05-16 23:58:36 +0530

    Done with fetch and adding loading

--- app/user/page.tsx
+++ app/user/page.tsx
@@ -1,7 +1,22 @@
-export default function User(){
+import axios from 'axios'
+
+async function fetchData() {
+    const response = await axios.get("https://week-13-offline.kirattechnologies.workers.dev/api/v1/user/details")
+
+    await new Promise(r => setTimeout(r, 1000))
+
+    return response.data;
+}
+
+
+export default async function User(){
+
+    const data = await fetchData();
+
     return (
         <div>
-            User file over here
+            {data.name} {" "}
+            {data.email}
         </div>
     )
 }
